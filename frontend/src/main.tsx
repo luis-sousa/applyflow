feef6a5b652428b66f4f0d8e90660b5fe75905ae
Luis Sousa <lmmsf.c.p@gmail.com>
2026-06-11 00:15:21 +0100

    build correction

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,14 @@
+if (typeof window !== 'undefined' && typeof window.CustomEvent !== 'function') {
+  const CustomEventPolyfill = function(event: string, params: CustomEventInit = { bubbles: false, cancelable: false, detail: null }) {
+    const evt = document.createEvent('CustomEvent')
+    evt.initCustomEvent(event, params.bubbles ?? false, params.cancelable ?? false, params.detail)
+    return evt
+  }
+
+  CustomEventPolyfill.prototype = Event.prototype
+  window.CustomEvent = CustomEventPolyfill as unknown as typeof CustomEvent
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
